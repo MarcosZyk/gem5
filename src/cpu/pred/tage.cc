805d4e27a7cd56ac2a0ceb7478d599912785fa1e
MarcosZyk <yukunzhou@YukundeMacBook-Air.local>
2025-05-26 23:08:56 +0800

    fix comment

--- src/cpu/pred/tage.cc
+++ src/cpu/pred/tage.cc
@@ -128,7 +128,7 @@ TAGE::lookup(ThreadID tid, Addr pc, void* &bp_history)
 
 void
 TAGE::updateHistories(ThreadID tid, Addr pc, bool uncond,
-                      bool taken, Addr target, void * &bp_history)
+                         bool taken, Addr target, void * &bp_history)
 {
     if (bp_history == nullptr) {
 
